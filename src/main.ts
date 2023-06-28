5948fe197fbc5992f248e990200347854199f76a
Carlos Albarracin <carlos8_alb@hotmail.com>
2023-06-28 13:25:51 -0300

    feat: change boostrap app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,31 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, provideFirebaseApp(() => initializeApp(environment.firebase)), provideAuth(() => {
+            const auth = getAuth();
+            //connectAuthEmulator(auth, 'http://localhost:9099', {
+            //  disableWarnings: true,
+            //});
+            return auth;
+        }), provideFirestore(() => {
+            const fireStore = getFirestore();
+            //connectFirestoreEmulator(fireStore, 'http://localhost', 9098)
+            return fireStore;
+        })),
+        provideHttpClient()
+    ]
+})
   .catch(err => console.error(err));
